5b3fbc0a476a0c47a75af9da0a7fc3b3d6540f09
John Blanchard <jnblanchard@mac.com>
2014-08-23 17:10:53 -0500

    making sure

--- GreenDoor/GreenDoor/GraphViewController.m
+++ GreenDoor/GreenDoor/GraphViewController.m
@@ -8,8 +8,8 @@
 
 #import "GraphViewController.h"
 
-@interface GraphViewController ()
 
+@interface GraphViewController ()
 @end
 
 @implementation GraphViewController
@@ -17,6 +17,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+//    self.graph = [[CPTGraph alloc] initWithFrame:self.view.frame];
+//    CPTGraphHostingView* hostView = (CPTGraphHostingView*)self.view;
+//    hostView.hostedGraph = self.graph;
+//    self.graph.paddingLeft = 20.0;
+//    self.graph.paddingTop = 20.0;
+//    self.graph.paddingRight = 20.0;
+//    self.graph.paddingBottom = 20.0;
+//
+//    CPTPlotSpace* plotSpace = (CPTPlotSpace*)self.graph.defaultPlotSpace;
 }
 
 
