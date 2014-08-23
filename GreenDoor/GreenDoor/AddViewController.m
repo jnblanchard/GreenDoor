6343835fb330a64b4686527202bac5b1c0bd334e
John Blanchard <jnblanchard@mac.com>
2014-08-23 14:22:58 -0500

    minor changes to add

--- GreenDoor/GreenDoor/AddViewController.m
+++ GreenDoor/GreenDoor/AddViewController.m
@@ -20,7 +20,6 @@
 @property BOOL validAmount;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 @property BOOL validDescription;
-@property BOOL validDate;
 @end
 
 @implementation AddViewController
@@ -32,17 +31,13 @@
     self.validAmount = NO;
     self.validItem = NO;
     self.validDescription = NO;
-    self.validDate = NO;
 }
 - (IBAction)incomeButton:(id)sender
 {
 
 }
 
-- (IBAction)dateChosen:(UIDatePicker *)sender
-{
-    self.validDate = YES;
-}
+
 
 - (IBAction)expenseButton:(id)sender
 {
@@ -51,6 +46,8 @@
         [alert show];
     } else {
         self.amountTextField.text = [@"-" stringByAppendingString:self.amountTextField.text];
+        self.amountTextField.textColor = [UIColor whiteColor];
+        self.amountTextField.backgroundColor = [UIColor redColor];
     }
 }
 
@@ -58,20 +55,23 @@
 {
     if ([sender isEqual:self.itemTextField] && ![sender.text isEqualToString:@""]) {
         self.validItem = YES;
+        NSLog(@"here1");
     }
     if ([sender isEqual:self.amountTextField] && ![sender.text isEqualToString:@""]) {
         self.validAmount = YES;
+        NSLog(@"here2");
     }
     if ([sender isEqual:self.self.descriptionTextField] && ![sender.text isEqualToString:@""]) {
         self.validDescription = YES;
         self.datePicker.hidden = NO;
+        NSLog(@"here3");
     }
     [sender resignFirstResponder];
 }
 
 - (IBAction)addItemAndAmount:(id)sender
 {
-    if(self.validAmount && self.validDescription && self.validItem && self.validDate)
+    if(self.validAmount && self.validDescription && self.validItem)
     {
         PFObject* aReport = [PFObject objectWithClassName:@"Report"];
         aReport[@"itemName"] = self.itemTextField.text;
@@ -83,7 +83,12 @@
         [aReport setObject:self.theUser forKey:@"user"];
         [aReport saveEventually:^(BOOL succeeded, NSError *error) {
             if (succeeded) {
-                
+                self.validItem = NO;
+                self.itemTextField.text = @"";
+                self.validAmount = NO;
+                self.amountTextField.text = @"";
+                self.validDescription = NO;
+                self.descriptionTextField.text = @"";
             }
         }];
     } else {
