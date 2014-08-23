95b640d106c3a8f5f918fdc00598d1f360435c62
Mohit Odhrani <Mohit@Mohit-Odhranis-MacBook-Pro.local>
2014-08-23 12:54:31 -0500

    settup project

---
+++ GreenDoor/GreenDoor/AddViewController.m
@@ -0,0 +1,57 @@
+//
+//  AddViewController.m
+//  GreenDoor
+//
+//  Created by Mohit Odhrani on 8/23/14.
+//  Copyright (c) 2014 John Blanchard. All rights reserved.
+//
+
+#import "AddViewController.h"
+
+@interface AddViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *itemTextField;
+@property (strong, nonatomic) IBOutlet UITextField *amountTextField;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *typeSegmentedControl;
+
+@property (strong, nonatomic) IBOutlet UITextField *descriptionTextField;
+@end
+
+@implementation AddViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)incomeButton:(id)sender {
+}
+- (IBAction)expenseButton:(id)sender {
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ GreenDoor/GreenDoor/CalenderViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalenderViewController.h
+//  GreenDoor
+//
+//  Created by Mohit Odhrani on 8/23/14.
+//  Copyright (c) 2014 John Blanchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalenderViewController : UIViewController
+
+@end
