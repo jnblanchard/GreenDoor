84c36a8c65d17ea4a04f1df34f08f2b30f1e348d
Ivan Ruiz <ivanruizmonjo@gmail.com>
2014-08-23 16:18:48 -0500

    putting the framework

--- GreenDoor/GreenDoor/MapViewController.m
+++ GreenDoor/GreenDoor/MapViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MapViewController.h"
+@import MapKit;
 
 @interface MapViewController ()
 
@@ -15,36 +16,13 @@
 
 @implementation MapViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.mapView.showsUserLocation = YES;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
