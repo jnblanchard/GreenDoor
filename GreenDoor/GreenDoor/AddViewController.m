52f7ac6b3fd43c0177f35629b004d9f9fa2a3572
John Blanchard <jnblanchard@mac.com>
2014-08-23 13:28:39 -0500

    minor change to storyboard

--- GreenDoor/GreenDoor/AddViewController.m
+++ GreenDoor/GreenDoor/AddViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "AddViewController.h"
+#import "Parse/Parse.h"
 
 @interface AddViewController ()
 @property (strong, nonatomic) IBOutlet UITextField *itemTextField;
 @property (strong, nonatomic) IBOutlet UITextField *amountTextField;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *typeSegmentedControl;
-
+@property PFUser* theUser;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *rateSegmentedControl;
 @property (strong, nonatomic) IBOutlet UITextField *descriptionTextField;
 @end
 
@@ -21,6 +23,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.theUser = [PFUser currentUser];
     // Do any additional setup after loading the view.
 }
 - (IBAction)incomeButton:(id)sender
