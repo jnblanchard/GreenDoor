77716a68bc9b5fd03a8d6454f4abad2a17c87dae
Ivan Ruiz <ivanruizmonjo@gmail.com>
2014-08-23 22:52:51 -0500

    added tabbar buttons

---
+++ GreenDoor/GreenDoor/NewsFeedCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsFeedCell.h
+//  GreenDoor
+//
+//  Created by Ivan Ruiz Monjo on 23/08/14.
+//  Copyright (c) 2014 John Blanchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsFeedCell : UITableViewCell
+
+@end
