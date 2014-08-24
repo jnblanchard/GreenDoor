03823cc5122571d06380063a07f609c173b3f90f
John Blanchard <jnblanchard@mac.com>
2014-08-23 22:38:09 -0500

    first tabbar

---
+++ GreenDoor/GreenDoor/OurBarTabViewController.h
@@ -0,0 +1,13 @@
+//
+//  OurBarTabViewController.h
+//  GreenDoor
+//
+//  Created by John Blanchard on 8/23/14.
+//  Copyright (c) 2014 John Blanchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OurBarTabViewController : UITabBarController
+
+@end
