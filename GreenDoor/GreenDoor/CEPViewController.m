8673fe6b53357b78bb2886ef5f0fb5669a7ee67c
Ivan Ruiz <ivanruizmonjo@gmail.com>
2014-08-24 13:22:44 -0500

    iv storyboard

--- GreenDoor/GreenDoor/CEPViewController.m
+++ GreenDoor/GreenDoor/CEPViewController.m
@@ -5,12 +5,16 @@
 //  Created by Ivan Ruiz Monjo on 23/08/14.
 //  Copyright (c) 2014 John Blanchard. All rights reserved.
 //
-
+#import <Parse/Parse.h>
 #import "CEPViewController.h"
+#import <MessageUI/MessageUI.h>
 @import MapKit;
 
-@interface CEPViewController ()
+@interface CEPViewController () <MFMailComposeViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *daySegmentedControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *hourSegmentedControl;
+@property (weak, nonatomic) IBOutlet UITextField *detailsTextField;
 
 @end
 
@@ -38,5 +42,47 @@
 
 - (IBAction)makeAppointment:(id)sender
 {
+    NSString *emailTitle = [NSString stringWithFormat:@"Appointment for CEP: %@",[PFUser currentUser].username];
+    // Email Content
+    NSString *body = [self.hourSegmentedControl titleForSegmentAtIndex:self.hourSegmentedControl.selectedSegmentIndex];
+    [body stringByAppendingString:@" "];
+    [body stringByAppendingString:[self.hourSegmentedControl titleForSegmentAtIndex:self.daySegmentedControl.selectedSegmentIndex]];
+    NSString *messageBody = body;
+    // To address
+    NSArray *toRecipents = [NSArray arrayWithObject:@"ivanruizmonjo@gmail.com"];
+
+    MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+    mc.mailComposeDelegate = self;
+    [mc setSubject:emailTitle];
+    [mc setMessageBody:messageBody isHTML:NO];
+    [mc setToRecipients:toRecipents];
+
+    // Present mail view controller on screen
+    [self presentViewController:mc animated:YES completion:NULL];
+}
+
+
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+
+    // Close the Mail Interface
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 @end
