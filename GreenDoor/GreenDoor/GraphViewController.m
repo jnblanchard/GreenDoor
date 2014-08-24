87d979927924982accd12374a43fe97a0093a00f
John Blanchard <jnblanchard@mac.com>
2014-08-23 21:33:50 -0500

    line graph

--- GreenDoor/GreenDoor/GraphViewController.m
+++ GreenDoor/GreenDoor/GraphViewController.m
@@ -13,6 +13,7 @@
 
 @interface GraphViewController () <SChartDatasource>
 @property ShinobiChart* chart;
+@property ShinobiChart* chartTwo;
 @property NSArray* dataArray;
 @property NSDate* minDate;
 @property NSDate* maxDate;
@@ -44,7 +45,7 @@
         self.minDate = [self findMinDate:array];
         self.maxDate = [self findMaxDate:array];
         CGFloat margin = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) ? 10.0 : 50.0;
-        self.chart = [[ShinobiChart alloc] initWithFrame:CGRectInset(CGRectMake(self.view.bounds.origin.x-10, self.view.bounds.origin.y+20, self.view.bounds.size.width+15, self.view.bounds.size.height - 60), margin, margin)];
+        self.chart = [[ShinobiChart alloc] initWithFrame:CGRectInset(CGRectMake(self.view.bounds.origin.x-10, self.view.bounds.origin.y+84, self.view.bounds.size.width+18, self.view.bounds.size.height - 125), margin, margin)];
         self.chart.title = @"Reports: Line Graph";
 
         self.chart.licenseKey = @"Yo4qzAHywKn0qvVMjAxNDA5MjJqbmJsYW5jaGFyZEBtYWMuY29trMV1GXfqeYP4GjjsB1dDDbPUmHVSHQkJAJQqpKM6feF5BrUFY8k9aaK4InUNRfCtQT+EgT4I851spCJLFzBtBEy/lawg0mAxLWtfyqR8Qw5EeWVuZkc37t0qyQeAlOmFrzGe/8eidlnpqaSLbS5xHt0bRNuM=BQxSUisl3BaWf/7myRmmlIjRnMU2cA7q+/03ZX9wdj30RzapYANf51ee3Pi8m2rVW6aD7t6Hi4Qy5vv9xpaQYXF5T7XzsafhzS3hbBokp36BoJZg8IrceBj742nQajYyV7trx5GIw9jy/V6r0bvctKYwTim7Kzq+YPWGMtqtQoU=PFJTQUtleVZhbHVlPjxNb2R1bHVzPnh6YlRrc2dYWWJvQUh5VGR6dkNzQXUrUVAxQnM5b2VrZUxxZVdacnRFbUx3OHZlWStBK3pteXg4NGpJbFkzT2hGdlNYbHZDSjlKVGZQTTF4S2ZweWZBVXBGeXgxRnVBMThOcDNETUxXR1JJbTJ6WXA3a1YyMEdYZGU3RnJyTHZjdGhIbW1BZ21PTTdwMFBsNWlSKzNVMDg5M1N4b2hCZlJ5RHdEeE9vdDNlMD08L01vZHVsdXM+PEV4cG9uZW50PkFRQUI8L0V4cG9uZW50PjwvUlNBS2V5VmFsdWU+"; // TODO: add your trial licence key here!
@@ -59,7 +60,7 @@
         self.chart.yAxis = yAxis;
 
         [self.view addSubview:self.chart];
-        
+
         self.chart.datasource = self;
     }];
 }
