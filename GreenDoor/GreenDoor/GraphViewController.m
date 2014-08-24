88cc46cac7ee3c9c1f02e4526f7b57635d750b2f
John Blanchard <jnblanchard@mac.com>
2014-08-23 21:10:58 -0500

    line graph

--- GreenDoor/GreenDoor/GraphViewController.m
+++ GreenDoor/GreenDoor/GraphViewController.m
@@ -14,6 +14,9 @@
 @interface GraphViewController () <SChartDatasource>
 @property ShinobiChart* chart;
 @property NSArray* dataArray;
+@property NSDate* minDate;
+@property NSDate* maxDate;
+@property NSMutableArray* dateArray;
 @end
 
 @implementation GraphViewController
@@ -21,34 +24,93 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CGFloat margin = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) ? 10.0 : 50.0;
-    self.chart = [[ShinobiChart alloc] initWithFrame:CGRectInset(CGRectMake(self.view.bounds.origin.x, self.view.bounds.origin.y+20, self.view.bounds.size.width, self.view.bounds.size.height - 60), margin, margin)];
-    self.chart.title = @"Reports";
-
-    self.chart.licenseKey = @"Yo4qzAHywKn0qvVMjAxNDA5MjJqbmJsYW5jaGFyZEBtYWMuY29trMV1GXfqeYP4GjjsB1dDDbPUmHVSHQkJAJQqpKM6feF5BrUFY8k9aaK4InUNRfCtQT+EgT4I851spCJLFzBtBEy/lawg0mAxLWtfyqR8Qw5EeWVuZkc37t0qyQeAlOmFrzGe/8eidlnpqaSLbS5xHt0bRNuM=BQxSUisl3BaWf/7myRmmlIjRnMU2cA7q+/03ZX9wdj30RzapYANf51ee3Pi8m2rVW6aD7t6Hi4Qy5vv9xpaQYXF5T7XzsafhzS3hbBokp36BoJZg8IrceBj742nQajYyV7trx5GIw9jy/V6r0bvctKYwTim7Kzq+YPWGMtqtQoU=PFJTQUtleVZhbHVlPjxNb2R1bHVzPnh6YlRrc2dYWWJvQUh5VGR6dkNzQXUrUVAxQnM5b2VrZUxxZVdacnRFbUx3OHZlWStBK3pteXg4NGpJbFkzT2hGdlNYbHZDSjlKVGZQTTF4S2ZweWZBVXBGeXgxRnVBMThOcDNETUxXR1JJbTJ6WXA3a1YyMEdYZGU3RnJyTHZjdGhIbW1BZ21PTTdwMFBsNWlSKzNVMDg5M1N4b2hCZlJ5RHdEeE9vdDNlMD08L01vZHVsdXM+PEV4cG9uZW50PkFRQUI8L0V4cG9uZW50PjwvUlNBS2V5VmFsdWU+"; // TODO: add your trial licence key here!
-    self.chart.autoresizingMask = ~UIViewAutoresizingNone;
-
-    SChartNumberAxis *xAxis = [[SChartNumberAxis alloc] init];
-    self.chart.xAxis = xAxis;
-
-    SChartNumberAxis *yAxis = [[SChartNumberAxis alloc] init];
-    self.chart.yAxis = yAxis;
-
-    [self.view addSubview:self.chart];
-
-    self.chart.datasource = self;
+    [self loadData];
 
 }
 
 - (void)loadData
 {
     PFQuery* query = [PFQuery queryWithClassName:@"Report"];
+    [query orderByAscending:@"date"];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if (!error) {
             self.dataArray = objects;
             [self.chart reloadData];
         }
+        NSMutableArray* array = [NSMutableArray new];
+        for (PFObject* report in self.dataArray) {
+            [array addObject:report[@"date"]];
+        }
+        self.minDate = [self findMinDate:array];
+        self.maxDate = [self findMaxDate:array];
+        CGFloat margin = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) ? 10.0 : 50.0;
+        self.chart = [[ShinobiChart alloc] initWithFrame:CGRectInset(CGRectMake(self.view.bounds.origin.x-10, self.view.bounds.origin.y+20, self.view.bounds.size.width+15, self.view.bounds.size.height - 60), margin, margin)];
+        self.chart.title = @"Reports: Line Graph";
+
+        self.chart.licenseKey = @"Yo4qzAHywKn0qvVMjAxNDA5MjJqbmJsYW5jaGFyZEBtYWMuY29trMV1GXfqeYP4GjjsB1dDDbPUmHVSHQkJAJQqpKM6feF5BrUFY8k9aaK4InUNRfCtQT+EgT4I851spCJLFzBtBEy/lawg0mAxLWtfyqR8Qw5EeWVuZkc37t0qyQeAlOmFrzGe/8eidlnpqaSLbS5xHt0bRNuM=BQxSUisl3BaWf/7myRmmlIjRnMU2cA7q+/03ZX9wdj30RzapYANf51ee3Pi8m2rVW6aD7t6Hi4Qy5vv9xpaQYXF5T7XzsafhzS3hbBokp36BoJZg8IrceBj742nQajYyV7trx5GIw9jy/V6r0bvctKYwTim7Kzq+YPWGMtqtQoU=PFJTQUtleVZhbHVlPjxNb2R1bHVzPnh6YlRrc2dYWWJvQUh5VGR6dkNzQXUrUVAxQnM5b2VrZUxxZVdacnRFbUx3OHZlWStBK3pteXg4NGpJbFkzT2hGdlNYbHZDSjlKVGZQTTF4S2ZweWZBVXBGeXgxRnVBMThOcDNETUxXR1JJbTJ6WXA3a1YyMEdYZGU3RnJyTHZjdGhIbW1BZ21PTTdwMFBsNWlSKzNVMDg5M1N4b2hCZlJ5RHdEeE9vdDNlMD08L01vZHVsdXM+PEV4cG9uZW50PkFRQUI8L0V4cG9uZW50PjwvUlNBS2V5VmFsdWU+"; // TODO: add your trial licence key here!
+        self.chart.autoresizingMask = ~UIViewAutoresizingNone;
+
+        SChartDateRange* dateRange = [[SChartDateRange alloc]initWithDateMinimum:self.minDate andDateMaximum:self.maxDate];
+        SChartDateTimeAxis* xAxis = [[SChartDateTimeAxis alloc]initWithRange:dateRange];
+        self.chart.xAxis = xAxis;
+
+        SChartNumberRange* rangeY = [[SChartNumberRange alloc]initWithMinimum:[NSNumber numberWithInteger:-3000] andMaximum:[NSNumber numberWithInteger:3000]];
+        SChartNumberAxis *yAxis = [[SChartNumberAxis alloc] initWithRange:rangeY];
+        self.chart.yAxis = yAxis;
+
+        [self.view addSubview:self.chart];
+        
+        self.chart.datasource = self;
     }];
+}
+
+-(NSMutableArray*)orderArray:(NSMutableArray*)array
+{
+    NSMutableArray* newArray = [NSMutableArray new];
+    NSDate* min = array.firstObject;
+    while (array.count != 0) {
+        for (NSDate* date in array) {
+            if ([min isEqual:[date laterDate:min]]) {
+                min = date;
+            }
+        }
+        [newArray addObject:min];
+        [array removeObject:min];
+    }
+    return newArray;
+}
+
+-(NSDate*)findMinDate:(NSArray*)array
+{
+    NSDate* min = array.firstObject;
+    for (NSDate* date in array) {
+        if ([min isEqual:[date laterDate:min]]) {
+            min = date;
+        }
+    }
+    NSDate *currentDate = min;
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    [offsetComponents setDay:-1];
+    NSDate *nextDate = [gregorian dateByAddingComponents:offsetComponents toDate: currentDate options:0];
+    return nextDate;
+}
+
+-(NSDate*)findMaxDate:(NSArray*)array
+{
+    NSDate* max = array.firstObject;
+    for (NSDate* date in array) {
+        if (![max isEqual:[date laterDate:max]]) {
+            max = date ;
+        }
+    }
+    NSDate *currentDate = max;
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    [offsetComponents setDay:1];
+    NSDate *nextDate = [gregorian dateByAddingComponents:offsetComponents toDate: currentDate options:0];
+    return nextDate;
+
 }
 
 - (NSInteger)numberOfSeriesInSChart:(ShinobiChart *)chart
@@ -59,35 +121,55 @@
 -(SChartSeries *)sChart:(ShinobiChart *)chart seriesAtIndex:(NSInteger)index {
 
     SChartLineSeries *lineSeries = [[SChartLineSeries alloc] init];
+    lineSeries.baseline = [NSNumber numberWithInt:0];
 
-    // the first series is a cosine curve, the second is a sine curve
-    if (index == 0) {
-        lineSeries.title = [NSString stringWithFormat:@"y = cos(x)"];
-    } else {
-        lineSeries.title = [NSString stringWithFormat:@"y = sin(x)"];
-    }
+    SChartNumberRange* rangeX = [[SChartNumberRange alloc]initWithMinimum:[NSNumber numberWithInteger:0] andMaximum:[NSNumber numberWithInteger:400]];
+    SChartNumberRange* rangeY = [[SChartNumberRange alloc]initWithMinimum:[NSNumber numberWithInteger:-3000] andMaximum:[NSNumber numberWithInteger:3000]];
+    SChartDataSeries* series = [[SChartDataSeries alloc] init];
+
+    SChartLineSeriesStyle* style = [[SChartLineSeriesStyle alloc] init];
+    [style setLineWidth:[NSNumber numberWithInt:3]];
+    SChartPointStyle* pointStyle = [SChartPointStyle new];
+    pointStyle.color = [UIColor greenColor];
+    pointStyle.colorBelowBaseline = [UIColor redColor];
+    pointStyle.showPoints = YES;
+    [pointStyle setRadius:[NSNumber numberWithInt:10]];
+    [pointStyle setInnerRadius:[NSNumber numberWithInt:6]];
+    style.pointStyle = pointStyle;
+    [style setLineColor:[UIColor greenColor]];
+    [style setLineColorBelowBaseline:[UIColor redColor]];
+    [lineSeries setStyle:style];
+
+
+    [series setRangeX:rangeX];
+    [series setRangeY:rangeY];
+
+    [lineSeries setDataSeries:series];
 
     return lineSeries;
 }
 
-- (NSInteger)sChart:(ShinobiChart *)chart numberOfDataPointsForSeriesAtIndex:(NSInteger)seriesIndex {
-    return 100;
+- (NSInteger)sChart:(ShinobiChart *)chart numberOfDataPointsForSeriesAtIndex:(NSInteger)seriesIndex
+{
+    return self.dataArray.count;
 }
 
-- (id<SChartData>)sChart:(ShinobiChart *)chart dataPointAtIndex:(NSInteger)dataIndex forSeriesAtIndex:(NSInteger)seriesIndex {
+- (id<SChartData>)sChart:(ShinobiChart *)chart dataPointAtIndex:(NSInteger)dataIndex forSeriesAtIndex:(NSInteger)seriesIndex{
 
     SChartDataPoint *datapoint = [[SChartDataPoint alloc] init];
+    PFObject* report = [self.dataArray objectAtIndex:dataIndex];
+    NSDate* date = report[@"date"];
+
+
+
+    NSString* amount = report[@"amount"];
+    int amountInt = amount.intValue;
 
     // both functions share the same x-values
-    double xValue = dataIndex / 10.0;
-    datapoint.xValue = [NSNumber numberWithDouble:xValue];
+    datapoint.xValue = date;
 
     // compute the y-value for each series
-    if (seriesIndex == 0) {
-        datapoint.yValue = [NSNumber numberWithDouble:cosf(xValue)];
-    } else {
-        datapoint.yValue = [NSNumber numberWithDouble:sinf(xValue)];
-    }
+    datapoint.yValue = [NSNumber numberWithInt:amountInt];
 
     return datapoint;
 }
